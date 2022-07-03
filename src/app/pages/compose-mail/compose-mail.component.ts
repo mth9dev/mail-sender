57583a3d6666148f0bc431a0b3f50f6743fd2ff4
mth9 <mth9dev@gmail.com>
2022-07-03 17:35:13 +0800

    Images Picker UI and Logic
    -Responsive image catalog.
    -Transform uploaded files and add to the form array.
    -Image count indicator.
    -remove images.

--- src/app/pages/compose-mail/compose-mail.component.ts
+++ src/app/pages/compose-mail/compose-mail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-compose-mail',
@@ -14,18 +14,44 @@ export class ComposeMailComponent implements OnInit {
     private _fb: FormBuilder,
   ) { }
 
+  get images() {
+    return this.mailForm.get('images') as FormArray;
+  }
+
   ngOnInit(): void {
     this.mailForm = this._fb.group({
       'firstName': [null, [Validators.required, Validators.pattern(/(?!^\s+$)^.*$/m)]],
       'lastName': [null, [Validators.required, Validators.pattern(/(?!^\s+$)^.*$/m)]],
       'email': [null, [Validators.required, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)]],
       'description': [null, [Validators.required, Validators.pattern(/(?!^\s+$)^.*$/m)]],
+      'images': this._fb.array([]),
     })
   }
 
   onSubmit() {
     this.mailForm.markAllAsTouched();
     console.log(this.mailForm);
+  }
+
+  addImage(event: Event) {
+    let fileList = (event.target as HTMLInputElement)?.files;
+    let fileArray = Array.from(fileList!);
+    for (let file of fileArray) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        let imageForm = this._fb.group({
+          name: file.name,
+          type: file.type,
+          url: (reader.result as string),
+        });
+        this.images.push(imageForm)
+      }
+      reader.readAsDataURL(file);
+    }
+  }
+
+  removeImage(index: number){
+    this.images.removeAt(index);
   }
 
 }
